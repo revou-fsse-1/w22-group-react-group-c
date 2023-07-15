1713cc3685f5e12631408cef18b3199fba9012b9
Dandi Rizky <dandirizkyy94@gmail.com>
2023-07-15 19:02:19 +0700

    add react player 3rd party

--- src/components/Homepage/Video.tsx
+++ src/components/Homepage/Video.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 
 export default function Video() {
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
   return (
     <div>
-      <video src="https://youtu.be/D-Ts7JfYVoU"></video>
+      {domLoaded && (
+        <div className="mt-10">
+          <ReactPlayer
+            muted={true}
+            loop={true}
+            playing={true}
+            url="https://youtu.be/D-Ts7JfYVoU"
+          />
+        </div>
+      )}
     </div>
   );
 }
