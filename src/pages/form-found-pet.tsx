7f83991aed5673cf3dc504acd2820f5d9feb12ba
Dandi Rizky <dandirizkyy94@gmail.com>
2023-07-15 23:54:49 +0700

    add found pet form

---
+++ src/pages/form-found-pet.tsx
@@ -0,0 +1,10 @@
+import FormFoundPet from "@/components/Form/FormFoundPet";
+import React from "react";
+
+export default function FoundPetFormRouter() {
+  return (
+    <div>
+      <FormFoundPet />
+    </div>
+  );
+}
