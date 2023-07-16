8a00f039f62ef98fd79787ffca7e4507c5cf0d84
Dandi Rizky <dandirizkyy94@gmail.com>
2023-07-16 22:08:01 +0700

    add edit find form feature

---
+++ src/pages/edit-form-find-pet.tsx
@@ -0,0 +1,10 @@
+import EditFindPetForm from "@/components/Form/EditFormFindPet";
+import React from "react";
+
+export default function EditFindPetFormRoute() {
+  return (
+    <div>
+      <EditFindPetForm />
+    </div>
+  );
+}
