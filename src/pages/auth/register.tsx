3009b34f24d05e0e565afa3c18c683728c130a44
Dandi Rizky <dandirizkyy94@gmail.com>
2023-07-16 13:05:10 +0700

    register and login functionality works

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 
 interface FormProps {
+  name: string;
   email: string;
   password: string;
 }
@@ -16,6 +17,7 @@ export default function Register() {
 
   const schema = yup
     .object({
+      name: yup.string().required("Name is required"),
       email: yup.string().email().required("Email is required"),
       password: yup.string().min(5).required("Password is required"),
     })
@@ -35,6 +37,7 @@ export default function Register() {
       await axios.post(
         "https://wheremypets-backend-production.up.railway.app/auth/register/user",
         {
+          name: data.name,
           email: data.email,
           password: data.password,
         }
@@ -53,10 +56,32 @@ export default function Register() {
           <div className="flex w-[500px]  flex-col space-y-5 rounded-lg border py-20 px-5 shadow-xl mx-auto">
             <div className="mx-auto mb-2 space-y-3">
               <h1 className=" text-3xl font-bold text-gray-700">
-                Register Toko-Ku
+                Register Form
               </h1>
             </div>
             <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <div className="relative mt-2 w-full">
+                  <input
+                    type="text"
+                    id="name"
+                    {...register("name")} // Register email input
+                    className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                    placeholder=" "
+                  />
+                  <label
+                    htmlFor="name"
+                    className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600"
+                  >
+                    {" "}
+                    Enter Your Name{" "}
+                  </label>
+                </div>
+              </div>
+              {errors.name && (
+                <p className="text-red-500 text-sm">{errors.name.message}</p>
+              )}
+
               <div>
                 <div className="relative mt-2 w-full">
                   <input
