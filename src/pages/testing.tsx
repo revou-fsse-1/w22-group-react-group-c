0c518626d0607223f01b7ed46300d7b53bc3d955
Dandi Rizky <dandirizkyy94@gmail.com>
2023-07-17 22:00:22 +0700

    testing fetch

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import React, { useState } from "react";
-interface Found {
+import React, { useEffect, useState } from "react";
+interface Content {
+  id: string;
   title: string;
   name: string;
   description: string;
@@ -9,102 +10,58 @@ interface Found {
   locationDetail: string;
   species: string;
   contact: string;
+  isFound: boolean;
   userId: string;
+  createdAt: string;
+  updateAt: string;
 }
 export default function Testing() {
-  const [title, setTitle] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [image, setImage] = useState("");
-  const [location, setLocation] = useState("");
-  const [locationDetail, setLocationDetail] = useState("");
-  const [species, setSpecies] = useState("");
-  const [contact, setContact] = useState("");
-  const [userId, setUserId] = useState("");
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const response = await axios.post("http://localhost:4000/found", {
-      title: title,
-      name: name,
-      description: description,
-      image: image,
-      location: location,
-      locationDetail: locationDetail,
-      species: species,
-      contact: contact,
-      userId: userId,
-    });
-    console.log(response);
+  const [content, setContent] = useState<Content[]>([]);
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   const response = await axios.post("http://localhost:4000/found", {
+
+  // };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://wheremypets-backend-production.up.railway.app/found"
+      );
+      setContent(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(content);
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={(event) => setTitle(event?.target.value)}
-        />
-        <label htmlFor="name">name</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(event) => setName(event?.target.value)}
-        />
-        <label htmlFor="description">description</label>
-        <input
-          type="text"
-          id="description"
-          value={description}
-          onChange={(event) => setDescription(event?.target.value)}
-        />
-        <label htmlFor="image">image</label>
-        <input
-          type="text"
-          id="image"
-          value={image}
-          onChange={(event) => setImage(event?.target.value)}
-        />
-        {/* <input type="file" name="" id="" /> */}
-        <label htmlFor="location">location</label>
-        <input
-          type="text"
-          id="location"
-          value={location}
-          onChange={(event) => setLocation(event?.target.value)}
-        />
-        <label htmlFor="locationDetail">location detail</label>
-        <input
-          type="text"
-          id="locationDetail"
-          value={locationDetail}
-          onChange={(event) => setLocationDetail(event?.target.value)}
-        />
-        <label htmlFor="species">species</label>
-        <input
-          type="text"
-          id="species"
-          value={species}
-          onChange={(event) => setSpecies(event?.target.value)}
-        />
-        <label htmlFor="contact">contact</label>
-        <input
-          type="text"
-          id="contact"
-          value={contact}
-          onChange={(event) => setContact(event?.target.value)}
-        />
-        <label htmlFor="userId">userId</label>
-        <input
-          type="text"
-          id="userId"
-          value={userId}
-          onChange={(event) => setUserId(event?.target.value)}
-        />
-        <button type="submit">submit</button>
-      </form>
+      {content.map((content) => (
+        <ul key={content.id}>
+          <li>{content.id}</li>
+          <li>{content.title}</li>
+          <li>{content.name}</li>
+          <li>{content.description}</li>
+          <img src={content.image} alt="image" />
+          <li>{content.location}</li>
+          <li>{content.locationDetail}</li>
+          <li>{content.species}</li>
+          <li>{String(content.isFound)}</li>
+          <li>{content.userId}</li>
+          <li>{content.createdAt}</li>
+          <li>{content.updateAt}</li>
+        </ul>
+      ))}
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
