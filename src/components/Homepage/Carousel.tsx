66ae2d054d6b3e40e4b47d60e3a7e51aca25585e
DandiRizkyy <dandirizkyy94@gmail.com>
2023-07-17 16:57:13 +0700

    changing carousel image

--- src/components/Homepage/Carousel.tsx
+++ src/components/Homepage/Carousel.tsx
@@ -3,19 +3,19 @@ import { Button, Carousel } from "@material-tailwind/react";
 export default function CarouselComponent() {
   return (
     <>
-      <Carousel className="rounded-xl mt-16 h-96">
+      <Carousel autoplay loop className="rounded-xl mt-16 h-96">
         <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          src="https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
           alt="image 1"
           className="h-full w-full object-cover"
         />
         <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          src="https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
           alt="image 2"
           className="h-full w-full object-cover"
         />
         <img
-          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+          src="https://images.unsplash.com/photo-1493101561740-e745892775b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
           alt="image 3"
           className="h-full w-full object-cover"
         />
