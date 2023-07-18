4ab3811b39c85a3b11fcd0bd934dfee4f5252dcf
aryaimmanuel <aryaimmanuel.acad@gmail.com>
2023-07-18 22:45:13 +0700

    add find pet list

---
+++ src/pages/get-find-pet-list.tsx
@@ -0,0 +1,10 @@
+import FindPetList from "@/components/PetList/FindPetList";
+import React from "react";
+
+export default function getFindPetListRoute() {
+  return (
+    <div>
+      <FindPetList />
+    </div>
+  );
+}
