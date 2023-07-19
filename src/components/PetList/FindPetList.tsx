0851cd0d580a2acf37f83beb3276f77bbf457b73
DandiRizkyy <dandirizkyy94@gmail.com>
2023-07-19 15:11:09 +0700

    editting find pet list responsive

--- src/components/PetList/FindPetList.tsx
+++ src/components/PetList/FindPetList.tsx
@@ -4,23 +4,26 @@ export default function FindPetList() {
   return (
     <div>
       {" "}
-      <div className="flex justify-center">
+      <div className="p-5 md:flex md:justify-center">
         <h1>Find Pet</h1>
-        <div className=" mx-auto grid grid-cols-1 sm:grid-cols-2 gap-11 mt-10 mb-20 ">
-          <div className="mt-20 max-w-lg rounded overflow-hidden shadow-lg">
+        <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 gap-11 mt-10 mb-20 ">
+          <div className="mt-20 sm:max-w-sm md:max-w-lg rounded overflow-hidden shadow-lg">
             <img
               className="w-full"
               src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=843&q=80"
               alt="cat"
             />
-            <div className="px-6 py-4">
+            <div className="px-1 md:px-6 py-4">
               <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
               <p className="text-gray-700 text-base pb-6">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Voluptatibus quia, nulla! Maiores et perferendis eaque,
                 exercitationem praesentium nihil.
               </p>
-              <div className="text-center font-bold">Read More...</div>
+              <p>
+                Status: <span className="font-bold">Belum Ditemukan</span>
+              </p>
+              <div className="text-center font-bold mt-5">Read More...</div>
             </div>
           </div>
           <div className="mt-20 max-w-lg rounded overflow-hidden shadow-lg">
@@ -36,7 +39,10 @@ export default function FindPetList() {
                 Voluptatibus quia, nulla! Maiores et perferendis eaque,
                 exercitationem praesentium nihil.
               </p>
-              <div className="text-center font-bold">Read More...</div>
+              <p>
+                Status: <span className="font-bold">Belum Ditemukan</span>
+              </p>
+              <div className="text-center font-bold mt-5">Read More...</div>
             </div>
           </div>
         </div>
